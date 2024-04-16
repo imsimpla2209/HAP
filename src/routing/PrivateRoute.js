c012c5637aa3f2ec91abc06504c53acc9aa41a71
nguyenhuyhoang <hoangnguyensp2209@gmail.com>
2024-04-16 19:02:27 +0700

    getTokenFromLocalStorage

--- src/routing/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const PrivateRoutes = ({ children, role }) => {
-    const getTokenFromLocalStorage = JSON.parse(localStorage.getItem("customer"))
-    return getTokenFromLocalStorage?.token !== undefined
-        && getTokenFromLocalStorage?.role === role
-        ? children : (<Navigate to='/login' replace={true} />)
-}
