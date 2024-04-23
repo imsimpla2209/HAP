e9b091ba192332c3584fb0aad4b2f0447b2f14bd
nguyenhuyhoang <hoangnguyensp2209@gmail.com>
2024-04-23 09:35:34 +0700

    add bg

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 const BreadCrumb = (props) => {
   const { title } = props;
   const divStyle = {
-    backgroundImage: `url(${props?.imageUrl})`,
+    backgroundImage: props?.imageUrl ? `url(${props?.imageUrl})` : 'url("images/blackbg6.jpeg")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     width: '100%',
     height: '75px', // Adjust this as per your requirement
+    backgroundColor: "#000000",
+
   };
 
   return (
-    <div className='breadscrumb mb-0 py-4' style={props?.imageUrl ? divStyle : {
-      backgroundColor: "#000000",
-    }}>
+    <div className='breadscrumb mb-0 py-4' style={divStyle}>
       <div className="container-xxl">
         <div className="row">
           <div className="col-12">
