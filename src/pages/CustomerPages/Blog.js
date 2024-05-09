301db8d2862a45651530a610b883c9a2aa084d11
Rankrook <rankrook@gmail.com>
2024-05-09 16:48:27 +0700

    update page blog

--- src/pages/CustomerPages/Blog.js
+++ src/pages/CustomerPages/Blog.js
@@ -40,7 +40,7 @@ const Blog = () => {
                         postId={item?.postId}
                         title={item?.title}
                         content={item?.content}
-                        image={item?.thumbnail}
+                        thumbnail={item?.thumbnail}
                         // "https://img.freepik.com/premium-vector/global-network-connection-world-map-point-line-composition-concept-global-business-internet-technology-social-network-vector-illustration_230610-458.jpg"
                       />
                     </div>
